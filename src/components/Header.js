c28d8acea2a697a696e4e3c2dcbb1383815661ee
葉嘉文 <mikeye@local>
2022-06-21 01:41:56 +0800

    feat: add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+
+function Header() {
+  return (
+    <HeaderContainer>
+      {/* HeaderLeft */}
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      {/* Search */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input type="text" placeholder="Search" />
+      </HeaderSearch>
+
+      {/* HeaderRight */}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
+    </HeaderContainer>
+  )
+}
+
+export default Header
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  flex: .3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: .8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: .4;
+  display: flex;
+  opacity: 1;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+  border-radius: 6px;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    text-align: center;
+    min-width: 30vw;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: .3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
